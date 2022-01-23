82c6f78bbcdeb854f454adb90b454575f80f831d
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2022-01-23 14:27:11 +0300

    Update

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,22 +1,18 @@
 export default class Character {
-  constructor(name) {
-    this.name = name;
-    this.type = 'type';
-    this.health = 100;
-    this.level = 1;
-    this.attack = 0;
-    this.defence = 0;
-  }
-
-  checkName(name) {
-  	if (this.name.length < 2 || this.name.length > 10) {
+  constructor(name, type) {
+    if (name.length < 2 || name.length > 10) {
   		throw new Error('Ошибка! Имя персонажа должно быть длиной от 2 до 10 символов!');
+  	} else {
+  		this.name = name;
   	}
-  }
-
-  checkType(type) {
-  	if (typeof this.type !== 'string') {
+  	if (typeof type !== 'string') {
   		throw new Error('Ошибка! Тип персонажа не может быть числом!');
+  	} else if (type !== 'bowman' && type !== 'swordsman' && type !== 'magician' && type !== 'daemon' && type !== 'undead' && type !== 'zombie') {
+  		throw new Error('Ошибка! Неверный тип персонажа!');
+  	} else {
+  		this.type = type;
   	}
+  	this.health = 100;
+    this.level = 1;
   }
 }
