46038d3c31006b660e3ef1355eb31ed6fbcab24d
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2022-01-25 21:30:50 +0300

    UpdateUpdate

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,13 +1,13 @@
 export default class Character {
+  static types = ['bowman', 'swordsman', 'magician', 'daemon', 'undead', 'zombie'];
+
   constructor(name, type) {
     if (name.length < 2 || name.length > 10) {
   		throw new Error('Ошибка! Имя персонажа должно быть длиной от 2 до 10 символов!');
   	} else {
   		this.name = name;
   	}
-  	if (typeof type !== 'string') {
-  		throw new Error('Ошибка! Тип персонажа не может быть числом!');
-  	} else if (type !== 'bowman' && type !== 'swordsman' && type !== 'magician' && type !== 'daemon' && type !== 'undead' && type !== 'zombie') {
+  	if (!Character.types.includes(type)) {
   		throw new Error('Ошибка! Неверный тип персонажа!');
   	} else {
   		this.type = type;
